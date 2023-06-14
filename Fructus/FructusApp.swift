3168ecfad58e5617b7546315569e1cc105d438ec
Aslanyan99n <a99narek@gmail.com>
2023-06-14 10:52:16 +0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Narek on 14.06.23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
